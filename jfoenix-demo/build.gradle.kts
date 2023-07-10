8f57b1a90077bdbdbc40b28a520df97b67129949
Sylvain Bertrand <sbertrand@ihmc.org>
2023-07-10 17:27:15 -0500

    Initial import

---
+++ jfoenix-demo/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+   id("us.ihmc.ihmc-build")
+   id("us.ihmc.ihmc-ci") version "8.3"
+   id("us.ihmc.ihmc-cd") version "1.24"
+}
+
+ihmc {
+   loadProductProperties("../group.gradle.properties")
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+mainDependencies {
+   api("us.ihmc:jfoenix:source")
+}
+
+testDependencies {
+}
---
+++ jfoenix/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+   id("us.ihmc.ihmc-build")
+   id("us.ihmc.ihmc-ci") version "8.3"
+   id("us.ihmc.ihmc-cd") version "1.24"
+}
+
+ihmc {
+   loadProductProperties("../group.gradle.properties")
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+mainDependencies {
+   var javaFXVersion = "17.0.2"
+   api(ihmc.javaFXModule("base", javaFXVersion))
+   api(ihmc.javaFXModule("controls", javaFXVersion))
+   api(ihmc.javaFXModule("graphics", javaFXVersion))
+   api(ihmc.javaFXModule("fxml", javaFXVersion))
+   api(ihmc.javaFXModule("swing", javaFXVersion))
+}
+
+testDependencies {
+}
